9aaacc3e5efa2a1e6706ad77e2fccee6ba5adc37
Ismail Dinc <35398477+iso57@users.noreply.github.com>
2020-11-05 19:51:48 +0100

    upload'

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	let todos = [];
+	let newTodo;
+	function addTodo(event) {
+		todos = [...todos,newTodo];
+		newTodo = "";
+	}
+</script>
+
+<main>
+	TODOLIST
+
+	<input type="text" bind:value={newTodo}>
+	<button on:click={addTodo}>Ajouter</button>
+	
+	{#if todos.length > 0}
+	Todos
+	<ul>
+	{#each todos as todo }
+	<li>{todo}</li>
+		
+	{/each}
+		</ul>
+	{/if}
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
